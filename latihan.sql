e7f04ef8396405374a3233618a6b1ea07025aee7
Trio Ferdiansyah <ferdiaddawy@yahoo.com>
2016-04-12 07:45:44 +0700

    Added files via upload

---
+++ latihan.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 12, 2016 at 02:44 AM
+-- Server version: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_latihan`
+--
+
+CREATE TABLE `tbl_latihan` (
+  `id` int(5) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `telp` varchar(13) NOT NULL,
+  `foto` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_latihan`
+--
+
+INSERT INTO `tbl_latihan` (`id`, `nama`, `email`, `telp`, `foto`) VALUES
+(1, 'Trioo', 'trio@gmail.com', '098928376786', 'trio2.jpg'),
+(6, 'asas', 'kalal@jsajskjkkss.com', '878789', 'trio6.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_latihan`
+--
+ALTER TABLE `tbl_latihan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_latihan`
+--
+ALTER TABLE `tbl_latihan`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
